38604442bd7770ec67aab7d6f31403f35417c129
Vladislav <vladislavprokhorov1005@gmail.com>
2026-02-07 21:07:18 +0200

    Created footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Hero from '../sections/Hero'
 import About from '../sections/About'
 import Contact from '../sections/Contact'
+import Footer from '../sections/Footer'
+import Projects from '../sections/Projects'
 
 export default function Home() {
   return (
     <div className='bg-[rgb(var(--bg))] '>
       <Hero />
       <About />
+      <Projects />
       <Contact />
+      <Footer />
     </div>
   )
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='bg-gray-950 text-gray-300/40 text-sm flex flex-col items-center pb-[100px]'>
+      <p>© 2026 Владислав Прохоров</p>
+      <p>Engineering quality into every line of code.</p>
+    </footer>
   )
 }
